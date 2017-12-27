0829f418e64918f333fe04a2e5376c950b6ee586
sabreen.99 <sabreen.ahmed.222@gmail.com>
2017-12-27 09:16:54 +0200

    hide links

--- spec/features/users/signing_users_in_spec.rb
+++ spec/features/users/signing_users_in_spec.rb
@@ -12,7 +12,10 @@ RSpec.feature "usrers sign in" do
     fill_in "Password", with: @john.password
     click_button "Log in"
 
-    expect(page). to have_content("Signed in successfully.")
-    expect(page). to have_content("Signed in as #{@john.email}")
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{@john.email}")
+    expect(page).to have_link("Sign out")
+    expect(page).not_to have_link("Sign up")
+    expect(page).not_to have_link("Sign in")
   end
 end
