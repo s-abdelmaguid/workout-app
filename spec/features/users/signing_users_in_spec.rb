0bcbbeced41b326b1d5bd3898a81c74da28480a7
sabreen.99 <sabreen.ahmed.222@gmail.com>
2017-12-31 13:22:13 +0200

    fixing broken specs

--- spec/features/users/signing_users_in_spec.rb
+++ spec/features/users/signing_users_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "usrers sign in" do
   before do
-    @john = User.create!(email: "john@example.com", password: "password")
+    @john = User.create!(first_name: "john", last_name: "Doe", email: "john@example.com", password: "password")
   end
 
   scenario "with valid credetials" do
