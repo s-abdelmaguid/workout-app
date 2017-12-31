533ce9957b275569fd95c2e2ce309e630d5cc8c9
sabreen.99 <sabreen.ahmed.222@gmail.com>
2017-12-31 13:00:03 +0200

    Add first and last name to dign up form

--- spec/features/exercises/deleting_exercise_spec.rb
+++ spec/features/exercises/deleting_exercise_spec.rb
@@ -18,7 +18,7 @@ RSpec.feature "Deleting Exercise" do
 
     path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}"
     link = "//a[contains(@href,\'#{path}\') and .//text()='Destroy']"
-    
+
     find(:xpath, link).click
 
     expect(page).to have_content("Exercise has been deleted")
--- spec/features/signing_user_up_spec.rb
+++ spec/features/signing_user_up_spec.rb
@@ -4,12 +4,15 @@ RSpec.feature "User Signup" do
   scenario "vaild credentials" do
     visit "/"
     click_link("Sign up")
-
+    fill_in "First name" , with: "john"
+    fill_in "Last name" , with: "Doe"
     fill_in "Email" , with: "john@example.com"
     fill_in "Password", with: "Password"
     fill_in "Password confirmation", with: "Password"
     click_button("Sign up")
 
     expect(page).to have_content("You have signed up successfully.")
+    visit "/"
+    expect(page).to have_content("John Doe")
   end
 end
