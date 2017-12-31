fa6c7ad0312c98eeaab12f99ee049dd93a54e5dd
sabreen.99 <sabreen.ahmed.222@gmail.com>
2017-12-31 13:08:31 +0200

    validates first and last name

--- spec/features/signing_user_up_spec.rb
+++ spec/features/signing_user_up_spec.rb
@@ -1,18 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature "User Signup" do
-  scenario "vaild credentials" do
+RSpec.feature "Users signup" do
+  scenario "with valid credentials" do
     visit "/"
-    click_link("Sign up")
-    fill_in "First name" , with: "john"
-    fill_in "Last name" , with: "Doe"
-    fill_in "Email" , with: "john@example.com"
-    fill_in "Password", with: "Password"
-    fill_in "Password confirmation", with: "Password"
-    click_button("Sign up")
+
+    click_link "Sign up"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
+    click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully.")
+
+    user = User.last
+    room = user.room
+    room_name = user.full_name.split.join('-')
+    expect(room.name).to eq(room_name)
+
     visit "/"
     expect(page).to have_content("John Doe")
   end
+
+  scenario "with invalid credentials" do
+    visit "/"
+
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
+
 end
